bbb9585d340e69c3ab73b147c7fdce0b394080e7
Jay Yu <ygh970627@gmail.com>
2026-06-07 21:10:04 -0500

    ADD: image, bgcolor

--- src/component/introVideo/IntroVideo.tsx
+++ src/component/introVideo/IntroVideo.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 type Props = {
     onFinished: () => void
 }
@@ -5,14 +7,27 @@ type Props = {
 export default function IntroVideo({
     onFinished,
 }: Props) {
+    const [fadeOut, setFadeOut] = useState(false)
+
+    const handleEnd = () => {
+        setFadeOut(true)
+
+        setTimeout(() => {
+            onFinished()
+        }, 1000) // CSS transition 시간과 맞춤
+    }
+
     return (
-        <div className="intro-container">
+        <div
+            className={`intro-container ${fadeOut ? "fade-out" : ""
+                }`}
+        >
             <video
                 autoPlay
                 muted
                 playsInline
                 className="intro-video"
-                onEnded={onFinished}
+                onEnded={handleEnd}
             >
                 <source
                     src="/videos/slot machine_motion_ivory background.mp4"
--- src/component/shareButton/index.tsx
+++ src/component/shareButton/index.tsx
@@ -44,7 +44,7 @@ export const ShareButton = () => {
                 "//" +
                 window.location.host +
                 baseUrl +
-                "/preview_image.png",
+                "/YG_04833.webp",
               link: {
                 mobileWebUrl:
                   window.location.protocol +
